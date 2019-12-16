62d4ec10f1ee3d98865976a7f6f4e7665bb1bf9a
Andreas Gerstmayr <agerstmayr@redhat.com>
2019-12-16 19:02:03 +0100

    build: remove uglify-webpack-plugin (deprecated)

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,14 +1,6 @@
 const baseWebpackConfig = require('./webpack.config');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 var conf = baseWebpackConfig;
 conf.mode = 'production';
-
-//conf.plugins.push(new ngAnnotatePlugin());
-// conf.plugins.push(
-//   new UglifyJSPlugin({
-//     sourceMap: true,
-//   })
-// );
 
 module.exports = conf;
