eed62e4df9be7eee61e3578a939d1b846b18a646
Andreas Gerstmayr <agerstmayr@redhat.com>
2020-07-09 19:49:00 +0200

    build: upgrade Grafana deps to version 7.0.6

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,6 @@ module.exports.getWebpackConfig = (config, options) => ({
         rules: [
             // worker-loader need to be processed before ts-loader
             {
-
                 test: /\.worker\.ts$/,
                 loader: 'worker-loader',
                 options: { publicPath: 'public/plugins/performancecopilot-pcp-app/' }
