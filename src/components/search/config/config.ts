893c63ff225df21140eb28472e9122e5b57e418e
Miroslav Foltýn <miroslav.foltyn64@gmail.com>
2020-07-22 23:09:39 +0200

    enabled autocomplete

--- src/components/search/config/config.ts
+++ src/components/search/config/config.ts
@@ -3,7 +3,7 @@ const Config = Object.freeze({
     MAX_SEARCH_SHORTCUTS: 12,
     REQUEST_TIMEOUT: 2500,
     RESULTS_PER_PAGE: 8,
-    ALLOW_SEARCH_SUGGESTIONS: false,
+    ALLOW_SEARCH_SUGGESTIONS: true,
 });
 
 export default Config;
