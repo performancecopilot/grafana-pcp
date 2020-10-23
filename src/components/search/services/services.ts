638b227a6fe0d2232a838086371f1a0ca553feef
Andreas Gerstmayr <agerstmayr@redhat.com>
2020-10-23 13:35:31 +0200

    search: find redis datasource by type, not by name

--- src/components/search/services/services.ts
+++ src/components/search/services/services.ts
@@ -1,9 +1,9 @@
-import { getBackendSrv, getDataSourceSrv } from '@grafana/runtime';
+import { getBackendSrv } from '@grafana/runtime';
 import PmSearchApiService from './PmSearchApiService';
 import EntityService from './EntityDetailService';
 import { PmSeriesApiService } from 'common/services/pmseries/PmSeriesApiService';
 import Config from '../config/config';
-import { isBlank } from 'common/utils';
+import redisPluginConfig from '../../../datasources/redis/plugin.json';
 
 export interface Services {
     searchService: PmSearchApiService;
@@ -11,33 +11,21 @@ export interface Services {
     entityService: EntityService;
 }
 
-const getDatasourceSettings = async (name: string) => {
-    let datasource: any;
-    try {
-        datasource = await getDataSourceSrv().get(name);
-    } catch (error) {
+async function getDatasourceSettings() {
+    const datasources = await getBackendSrv().get('/api/datasources');
+    const redisDatasource = datasources.find((ds: any) => ds.type === redisPluginConfig.id);
+    if (!redisDatasource) {
         throw new Error(
-            `${error.message}. Please create a datasource named '${name}' before using the search feature.`
+            `Could not find any PCP Redis datasource. Please create a PCP Redis datasource before using the search feature.`
         );
     }
-
-    const uid = datasource?.instanceSettings?.uid;
-    const settings = getDataSourceSrv().getDataSourceSettingsByUid(uid);
-    if (!settings || isBlank(settings.url)) {
-        throw new Error(
-            `Cannot get datasource settings of '${name}'. Please create this datasource before using the search feature.`
-        );
-    }
-    return settings;
-};
+    return redisDatasource;
+}
 
 export const initServices = async (): Promise<Services> => {
-    // PCP Redis is a backend datasource, and the instance settings can't be accessed client-side
-    const settings = await getDatasourceSettings('PCP Vector');
-    const backendSrv = getBackendSrv();
-
-    const searchService = new PmSearchApiService(settings, backendSrv);
-    const seriesService = new PmSeriesApiService(backendSrv, {
+    const settings = await getDatasourceSettings();
+    const searchService = new PmSearchApiService(settings, getBackendSrv());
+    const seriesService = new PmSeriesApiService(getBackendSrv(), {
         dsInstanceSettings: settings,
         isDatasourceRequest: false,
         baseUrl: settings.url!,
