3ec1fa7a29b1bfea9b41e3bab70c443438aef02a
Andreas Gerstmayr <agerstmayr@redhat.com>
2020-11-18 18:16:20 +0100

    test: fix e2e tests

--- jest.config.e2e.js
+++ jest.config.e2e.js
@@ -5,6 +5,8 @@ module.exports = {
     ...defaultConfig,
     setupFiles: ['jest-canvas-mock'],
     preset: 'jest-puppeteer',
+    // grafana-toolkit overrides this, and then "page" is undefined
+    testEnvironment: 'jest-environment-puppeteer',
     testMatch: [
         '<rootDir>/e2e/**/*.{ts,tsx}',
     ],
