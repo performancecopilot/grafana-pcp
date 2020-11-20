87b4b7230e34b4e7876b75410bc8e11625af5367
Andreas Gerstmayr <agerstmayr@redhat.com>
2020-11-20 20:00:53 +0100

    ci: sign plugin, create release zip
    Resolves #59

--- scripts/github-release.sh
+++ scripts/github-release.sh
@@ -2,24 +2,51 @@
 #
 # Release process adapted from
 # https://github.com/grafana/kentik-app/blob/master/.circleci/config.yml
+# https://github.com/marcusolsson/grafana-jsonapi-datasource/blob/master/.github/workflows/main.yml
 #
 
-VERSION=$(jq -r '.version' package.json)
+GRAFANA_PLUGIN_ID="$(jq -r '.id' dist/plugin.json)"
+GRAFANA_PLUGIN_TYPE="$(jq -r '.type' dist/plugin.json)"
+GRAFANA_PLUGIN_VERSION="$(jq -r '.info.version' dist/plugin.json)"
+GRAFANA_PLUGIN_SHA="$(git rev-parse HEAD)"
+GRAFANA_PLUGIN_ARTIFACT="${GRAFANA_PLUGIN_ID}-${GRAFANA_PLUGIN_VERSION}.zip"
+GRAFANA_PLUGIN_ARTIFACT_CHECKSUM="${GRAFANA_PLUGIN_ARTIFACT}.md5"
+
 RELEASE_NOTES=$(awk '/^## / {s++} s == 1 {print}' CHANGELOG.md)
 PRERELEASE_ARG=""
-
-if [[ "$VERSION" == *beta* ]]; then
+if [[ "${GRAFANA_PLUGIN_VERSION}" == *beta* ]]; then
   PRERELEASE_ARG="-p"
 fi
 
-# Grafana expects a dist directory in the commit specified by the Git tag
-git add --force dist/
-git commit -m "release $VERSION"
-git tag "v${VERSION}"
-git push origin "v${VERSION}"
+mv dist "${GRAFANA_PLUGIN_ID}"
+zip "${GRAFANA_PLUGIN_ARTIFACT}" "${GRAFANA_PLUGIN_ID}" -r
+md5sum "${GRAFANA_PLUGIN_ARTIFACT}" > "${GRAFANA_PLUGIN_ARTIFACT_CHECKSUM}"
+GRAFANA_PLUGIN_CHECKSUM=$(cut -d' ' -f1 "${GRAFANA_PLUGIN_ARTIFACT_CHECKSUM}")
 
 hub release create \
-    -m "grafana-pcp v${VERSION}" \
+    -m "grafana-pcp v${GRAFANA_PLUGIN_VERSION}" \
     -m "${RELEASE_NOTES}" \
+    -a "${GRAFANA_PLUGIN_ARTIFACT}" \
+    -a "${GRAFANA_PLUGIN_ARTIFACT_CHECKSUM}" \
     $PRERELEASE_ARG \
-    "v${VERSION}"
+    "v${GRAFANA_PLUGIN_VERSION}"
+
+echo Publish your plugin to grafana.com/plugins by opening a PR to https://github.com/grafana/grafana-plugin-repository with the following entry:
+jq <<EOF
+{
+    "id": "${GRAFANA_PLUGIN_ID}",
+    "type": "${GRAFANA_PLUGIN_TYPE}",
+    "url": "https://github.com/${GITHUB_REPOSITORY}",
+    "versions": [{
+        "version": "${GRAFANA_PLUGIN_VERSION}",
+        "commit": "${GRAFANA_PLUGIN_SHA}",
+        "url": "https://github.com/${GITHUB_REPOSITORY}",
+        "download": {
+            "any": {
+                "url": "https://github.com/${GITHUB_REPOSITORY}/releases/download/v${GRAFANA_PLUGIN_VERSION}/${GRAFANA_PLUGIN_ARTIFACT}",
+                "md5": "${GRAFANA_PLUGIN_CHECKSUM}"
+            }
+        }
+    }]
+}
+EOF
